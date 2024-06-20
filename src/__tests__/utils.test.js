e6bd02308310b18708bfa20f0bb2a1126c586fe7
Amene <layla_1380@yahoo.com>
2024-06-20 14:28:12 -0500

    unit test

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,12 @@
 // Your tests here
+import { pointsForWord } from '../utils'
+
+describe('pointsForWord', () => {
+    it('calculates the total points for a word(1 point for vowel, 2 per consonent)',
+        () => {
+            const word = 'test'
+            const points = pointsForWord(word)
+            expect(points).toBe(7)
+        
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,12 @@
 // Your code here
+export function pointsForWord(word) {
+    let result = 0
+    for (let letter of word) {
+        if (letter.includes('a') || letter.includes('e') || letter.includes('u') || letter.includes('o') || letter.includes('i')) {
+            result += 1
+        } else {
+            result +=2
+        }
+    }
+    return result
+}
